cbbe7f70ee2482e88e171ad2bd8ad0ba574cb6a3
francis <chicano@lcc.uma.es>
2021-05-23 22:28:39 +0200

    changed reference version

--- ewp/local-ewp/ewp-data/docker-entrypoint.sh
+++ ewp/local-ewp/ewp-data/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Configure EWP
-unzip /root/ewp-reference-connector-1.0-SNAPSHOT.war -d /usr/local/tomee/webapps/ewp
+unzip /root/ewp-reference-connector-2.0-SNAPSHOT.war -d /usr/local/tomee/webapps/ewp
 mkdir -p /usr/local/tomee/certs
 keytool -v -importkeystore -srckeystore /root/keys/ewp-local-uma.p12 -srcstoretype PKCS12 \
 	-destkeystore /usr/local/tomee/certs/ewp-local-uma.jks -deststoretype JKS \
--- ewp/setup.sh
+++ ewp/setup.sh
@@ -5,7 +5,7 @@ cp local-ewp/ewp-data/keys/local-ewp.pem local-registry/keys/certs_to_trust
 cp local-ewp/ewp-data/keys/httpsig.pem local-registry/keys/certs_to_trust
 cp local-registry/keys/cert.pem local-ewp/ewp-data/keys/certs_to_trust/registry.pem
 
-cp ../../ewp-reference-connector/ewp-reference-connector/target/ewp-reference-connector-1.0-SNAPSHOT.war local-ewp/ewp-data
+cp ../../ewp-reference-connector/ewp-reference-connector/target/ewp-reference-connector-2.0-SNAPSHOT.war local-ewp/ewp-data
 
 cd local-registry 
 ./setup.sh https://local.ewp.uma.es/ewp/rest/manifest
