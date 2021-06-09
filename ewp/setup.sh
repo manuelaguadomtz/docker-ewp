5042b8473f3a80930875422ab90e9153387c677d
francis <chicano@lcc.uma.es>
2021-06-09 17:27:30 +0200

    fixing some files

--- ewp/setup.sh
+++ ewp/setup.sh
@@ -13,7 +13,7 @@ cp local-ewp/ewp-data/keys/local-ewp.pem test-ewp/ewp-data/keys/certs_to_trust
 cp local-registry/keys/cert.pem local-ewp/ewp-data/keys/certs_to_trust/registry.pem
 cp local-registry/keys/cert.pem test-ewp/ewp-data/keys/certs_to_trust/registry.pem
 
-cp ../../ewp-reference-connector/ewp-reference-connector/target/ewp-reference-connector-2.0-SNAPSHOT.war local-ewp/ewp-data
+#cp ../../ewp-reference-connector/ewp-reference-connector/target/ewp-reference-connector-2.0-SNAPSHOT.war local-ewp/ewp-data
 
 cd local-registry 
 ./setup.sh https://local.ewp.uma.es/ewp/rest/manifest https://test.ewp.uma.es/ewp/rest/manifest
